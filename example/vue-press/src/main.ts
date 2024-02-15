14edc320737ce3aef945d8b8fbc5a05a8df028ee
Demis Bellot <demis.bellot@gmail.com>
2024-02-15 09:57:39 +0800

    handle external links

--- example/vue-press/src/main.ts
+++ example/vue-press/src/main.ts
@@ -19,6 +19,7 @@ const head = createHead()
 
 export const router = createRouter({
     history: createWebHistory(),
+    // Scroll to top on navigation
     scrollBehavior(to, _from, savedPosition) {
         if (savedPosition) {
             return savedPosition
@@ -43,6 +44,12 @@ export const router = createRouter({
         })
         return setupLayouts(routes)
     },
+})
+// handle external links
+router.beforeEach((to, _from, next) => {
+    if (to.path.startsWith('/http')) 
+        location.href = to.path.substring(1)
+    else next()
 })
 
 app
